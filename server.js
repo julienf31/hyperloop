85d8906b85667b7da35245d212316976160f65eb
julienf31 <julien.fournier@ynov.com>
2018-04-09 16:52:58 +0200

    sockets

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 var metro = require('./metro.js')
 var line = require('./line.js')
-var io = require('socket.io')
 
 app.set('view engine', 'ejs');
 
@@ -13,9 +15,22 @@ line.line.stations.forEach(function (station) {
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/socket.io-client/dist')); // SOCKET IO
+
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function (data) {
+        console.log(data);
+    });
+})
 
 app.get('/', function(req,res){
     res.render('index', { line: line.line})
 })
 
-app.listen(8080)
+
+server.listen(8080, function(){
+    console.log('listening on *:8080');
+});
