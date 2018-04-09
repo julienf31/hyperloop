40ad17c3c1aad42a968a2b6fc8280f1bd9af141c
julienf31 <julien.fournier@ynov.com>
2018-04-09 15:10:38 +0200

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'ejs');
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // CSS bootstrap
+
+app.get('/', function(req,res){
+    res.render('index')
+})
+
+app.listen(8080)
