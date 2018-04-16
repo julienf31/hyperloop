a81b3e30e37bbbc4a58edd2522c8e2300d4629f7
Olives Margot <olivesmargot@gmail.com>
2018-04-16 15:47:56 +0200

    Css link sur le index ejs

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ line.line.stations.forEach(function (station) {
     console.log('Station : ' + station.name)
 })
 
+
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // CSS bootstrap
