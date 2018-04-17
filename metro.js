8e51a358b13dc29700d390a0e49edc41016b1a46
julienf31 <julien.fournier@ynov.com>
2018-04-17 10:41:31 +0200

    speed management

--- metro.js
+++ metro.js
@@ -5,5 +5,71 @@ var metro = {
     speed: 60,
     numero: 243,
     faulty: false,
-    position: 1
+    position: 1,
+    acceleration: 1,
+    brake: -1,
+    emmergecyBrake: -3
+};
+
+
+speed = function(dist, io) {
+    maxSpeed = kmhToMs(metro.speed);
+    curDist = 0;
+    curSpeed = 0;
+    acceleration = metro.acceleration;
+    brake = metro.brake;
+    dT = .01;
+
+    var interval = setInterval(function () {
+        if(curDist < dist){
+            if(distToStop(curSpeed) >= dist - curDist){
+                //console.log('braking phase')
+                curDist += curSpeed*dT + ((brake)*(dT*dT))/2
+                curSpeed -= 1*dT
+                //console.log('speed : ' + curSpeed)
+                //console.log('dist : ' + curDist)
+            }else if(curSpeed < maxSpeed){
+                // ACCELERATION
+                curDist += curSpeed*dT + ((acceleration)*(dT*dT))/2
+                curSpeed += 1*dT
+                //console.log('acceleration')
+                //console.log('speed : ' + curSpeed)
+                //console.log('dist : ' + curDist)
+            }else{
+                curDist += curSpeed*dT
+                //console.log('speed : ' + curSpeed)
+                //console.log('dist : ' + curDist)
+            }
+            io.emit('speed', msToKmh(curSpeed))
+            io.emit('dist', curDist)
+        }
+        else{
+            console.log('Arrived to station')
+            clearInterval(interval)
+        }
+    }, dT*1000)
+
 }
+
+// Approximation de la distance d'arret
+function distToStop(speed){
+    return (speed*speed)/2
+}
+
+// Approximation de la distance d'arret d'urgence
+function distToEmergencyStop(speed){
+    return (speed*speed)/6
+}
+
+//Convert ms to kmh
+function msToKmh(speed) {
+    return speed * 3.6
+}
+
+//Convert kmh to ms
+function kmhToMs(speed) {
+    return speed / 3.6
+}
+
+exports.metro = metro;
+exports.speed = speed;
--- server.js
+++ server.js
@@ -1,36 +1,71 @@
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 var metro = require('./metro.js')
 var line = require('./line.js')
 
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/cu.usbmodem1421");
+
+
+port.on('open', function(){
+    console.log('Serial Port Opend');
+    port.on('data', function(data){
+        if(data[0] == 1){
+            io.emit('close', 1 , function () {
+                metro.speed(2000)
+            })
+        }if(data[0] == 2){
+            io.emit('open', 1)
+        }
+        console.log('data recieved' + data[0]);
+    });
+});
+
 app.set('view engine', 'ejs');
 
-line.line.stations.forEach(function (station) {
-    console.log('Station : ' + station.name)
-})
-
+app.use('/asset', express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // CSS bootstrap
-app.use('/js', express.static(__dirname + '/node_modules/socket.io-client/dist')); // SOCKET IO
 
-
-io.on('connection', function(client) {
-    console.log('Client connected...');
-
-    client.on('join', function (data) {
+io.on('connection', function(socket) {
+    socket.on('join', function (data) {
+    console.log('Client connected...')
         console.log(data);
-    });
+    })
+
+    socket.on('close', function (data) {
+        io.emit('close', data)
+        console.log('closing door of metro : ' + data);
+    })
 })
 
 app.get('/', function(req,res){
     res.render('index', { line: line.line})
+    metro.speed(1000, io)
+
 })
 
+//door management
+app.post('/door/:id/:status', function (req, res) {
+    if(req.params.status === 'close'){
+        io.emit('close', req.params.id)
+        console.log('metro : ' + req.params.id)
+        metro.speed(200)
+        req.pause()
+        res.status = 200
+        res.end('Door closing')
+    }else if(req.params.status === 'open'){
+        io.emit('open', req.params.id)
+        console.log('metro : ' + req.params.id)
+        req.pause()
+        res.status = 200
+        res.end('Door opening')
+    }
+})
 
-server.listen(8080, function(){
-    console.log('listening on *:8080');
-});
+server.listen(8080)
